ca74d080ca1dc7979b62de90a669baea52f3623c
V_Galaxy <1904821183@qq.com>
2023-04-07 22:53:16 +0800

    feat: add options and fetch button

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
+import hashlib
+import os
 import subprocess
+import urllib.request
 from typing import List
 
 import streamlit as st
 
+file_url = 'https://senti-strength.oss-cn-nanjing.aliyuncs.com/myEASIEST-1.0-SNAPSHOT.jar'
+file_name = 'myEASIEST-1.0-SNAPSHOT.jar'
 
-def cmd(text: str) -> List[str]:
-    return ["java", "-jar", "myEASIEST-1.0-SNAPSHOT.jar", "text", text, "explain"]
+
+def fetch() -> str:
+    os.remove(file_name)
+    urllib.request.urlretrieve(file_url, file_name)
+    with open(file_name, 'rb') as f:
+        md5 = hashlib.md5(f.read()).hexdigest()
+    return md5
 
 
-def fn(text: str) -> str:
-    result = subprocess.run(cmd(text), stdout=subprocess.PIPE, check=True)
+def cmd(text: str, option: str, explain: bool) -> List[str]:
+    res = ["java", "-jar", "myEASIEST-1.0-SNAPSHOT.jar", "text", text, option, ]
+    if explain:
+        res.append("explain")
+    return res
+
+
+def run(cmd: List[str]) -> str:
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, check=True)
     return result.stdout.decode('utf-8')
 
 
 if __name__ == "__main__":
-    st.title("SentiStrength")
-    user_input = st.text_input(label="Enter text:")
+    st.set_page_config(page_title="Senti Strength")
+    st.title("Senti Strength")
+    if st.button("Update JAR"):
+        st.info(f"Update successfully with latest MD5 **{fetch()}**")
+    st.write("---")
+    text = st.text_input(label="Enter text 👇")
+    option = st.radio(
+        "Set options 👇",
+        ('trinary', 'binary', 'scale'))
+    explain = st.checkbox('Explain')
     if st.button("Detect Sentiment"):
-        st.write(f"Result: {fn(user_input)}")
+        if len(text) == 0:
+            st.error("No input text")
+        else:
+            st.success(f"**Result:** {run(cmd(text, option, explain))}")
