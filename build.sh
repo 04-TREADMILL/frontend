5034a9a321d80f0493550199b97fbb5cce4b3c75
YuChen Ding <201250012@smail.nju.edu.cn>
2023-04-18 19:55:11 +0800

    build: update .gitlab-ci.yml

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 echo "*** BUILD START ***"
 sudo docker build -t registry.cn-shanghai.aliyuncs.com/vgalaxies/frontend .
-sudo docker stop `sudo docker ps -a | grep registry.cn-shanghai.aliyuncs.com/vgalaxies/frontend | awk '{print $1}'`
-sudo docker rm `sudo docker ps -a | grep registry.cn-shanghai.aliyuncs.com/vgalaxies/frontend | awk '{print $1}'`
+sudo docker stop `sudo docker ps -a | grep streamlit | awk '{print $1}'`
+sudo docker rm `sudo docker ps -a | grep streamlit | awk '{print $1}'`
 sudo docker run -d -p 8501:8501 registry.cn-shanghai.aliyuncs.com/vgalaxies/frontend
 echo "*** BUILD END ***"
